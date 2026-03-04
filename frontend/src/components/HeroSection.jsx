ae16e2ad2a60ca89f848c2189d0cf4ceffe21dcf
harshavardhanreddyjanga <harshavardhanreddyjanga02@gmail.com>
2026-03-04 16:14:41 +0530

    bg-update

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -7,7 +7,8 @@ import { useNavigate } from 'react-router-dom'
 function HeroSection() {
     const navigate = useNavigate()
   return (
-    <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("./backgroundImage.png")] bg-cover bg-center h-screen'>
+    <div  style={{ backgroundImage: `url(${assets.backgroundImage})` }}className = {` flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-cover bg-center h-screen `}>
+
          <img src={assets.marvelLogo} alt="" className='max-h-11 lg:h-11 mt-20' />
          <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'> Guardians <br /> of the Galaxy</h1>
 
